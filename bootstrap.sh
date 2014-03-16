5ce29db40a7b9829a5b235457e2c36a0c3c26b97
vagrant <vagrant@precise32.(none)>
2014-03-16 05:20:21 +0000

    Add gcc-4.8 config to vagrant file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,16 @@
 !/usr/bin/env bash
 
+apt-get install -y python-software-properties 
+add-apt-repository -y ppa:ubuntu-toolchain-r/test
+
 apt-get update
+apt-get install -y vim
+apt-get install -y git
+
+apt-get install -y gcc-4.8 g++-4.8
+update-alternatives --remove-all gcc 
+update-alternatives --remove-all g++
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 20
+update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 20
+update-alternatives --config gcc
+update-alternatives --config g++
