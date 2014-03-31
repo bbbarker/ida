63e43e9d56729c25e093c85de12e39c45c7d2ea4
Brian <bbarker5025@gmail.com>
2014-03-30 20:52:52 -0700

    set up ida to be a go project

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,7 @@ add-apt-repository -y ppa:ubuntu-toolchain-r/test
 
 apt-get update
 apt-get install -y vim
+apt-get install -y curl
 apt-get install -y git
 
 apt-get install -y gcc-4.8 g++-4.8
@@ -14,3 +15,7 @@ update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 20
 update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 20
 update-alternatives --config gcc
 update-alternatives --config g++
+
+curl -s 'http://go.googlecode.com/files/go1.2.1.linux-386.tar.gz' -o /tmp/go.tar.gz
+tar -C /usr/local -xzf /tmp/go.tar.gz
+export PATH=$PATH:/usr/local/go/bin
